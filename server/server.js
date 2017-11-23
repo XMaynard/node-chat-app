0373d4afa2c7368f39d815836b5a1e5e4db8a81b
BigO <course@africalendar.com>
2017-11-23 11:50:01 -0700

    greet new user and broadcast

--- server/server.js
+++ server/server.js
@@ -29,6 +29,20 @@ io.on('connection', (socket) =>{
         console.log('createEmail', newEmail);
     });
     
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to chat room',
+        createdAt: new Date().getTime()
+    });
+    
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+        
+    
+    
     socket.on('createMessage', (message) =>{
         console.log('createMessage', message);
         io.emit('newMessage', {
@@ -36,6 +50,12 @@ io.on('connection', (socket) =>{
             text: message.text,
             createdAt: new Date().getTime()
         });
+        
+//        socket.broadcast.emit('newMessage', {
+//           from: message.from,
+//            text: message.text,
+//            createAt: new Date().getTime()
+//        });
     });
     
     socket.on('disconnect', () => {
