3157abf29829221b3b26eb09f326f05f2907bfaa
BigO <course@africalendar.com>
2017-11-23 22:44:23 -0700

    add user form with jQuery

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,47 @@
 let socket = io();
     
     socket.on('connect', function(){
-       console.log('Connected to server'); 
-        //send to server from client/web form
-        socket.emit('createEmail', {
-           to: 'Jen@example.com',
-           subject: 'Hey. This is BigO'
-        });
+       console.log('Connected to server');   
     });
+
+socket.on('disconnect', function (){
+       console.log('Disconnected from the server'); 
+    });
+
+socket.on('newMessage', function(message){
+   console.log('New Message', message); 
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
     
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+    
+    socket.emit('createMessage', {
+        from: 'User',
+        text:jQuery('[name=message]').val()
+    }, function(){
+        
+    });
+});
+
 //receive from server
 //socket.on('newEmail', function(email){
 //   console.log('New email', email); 
 //});
 
-socket.on('newMessage', function(message){
-   console.log('New Message', message); 
-});
+//send to server from client/web form
+//    socket.emit('createEmail', {
+//           to: 'Jen@example.com',
+//           subject: 'Hey. This is BigO'
+//        });
 
-
-
-socket.on('disconnect', function (){
-       console.log('Disconnected from the server'); 
-    });
+//Automatically send message
+//socket.emit('createMessage', {
+//    from: 'Frank',
+//    text: 'Hello'
+//}, function(data){
+//    console.log('Got it', data);
+//});
