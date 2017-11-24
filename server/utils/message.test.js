0919ab111c5ee6fc2b105c01a9cb8a4c00778d6f
BigO <course@africalendar.com>
2017-11-24 00:18:27 -0700

    add geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 let expect = require('expect');
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () =>{
     it('should generate correct message object', () =>{
@@ -12,4 +12,18 @@ describe('generateMessage', () =>{
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
     });
+});
+
+describe('generateLocationMessage', () =>{
+   it('should generate correct location object', () =>{
+       let from = 'Bigo';
+       let latitude = 12;
+       let longitude = -97;
+       let url = 'https://www.google.com/maps?q=12,-97'
+       let location = generateLocationMessage(from,latitude, longitude);
+       
+       expect(location.createAt).toBeA('number');
+       expect(location).toInclude({from, url});
+       
+   }); 
 });
