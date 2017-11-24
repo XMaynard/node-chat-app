ef83779b17148e44a20adce9fa355e73ef5808b8
BigO <course@africalendar.com>
2017-11-24 11:46:44 -0700

    add mustachejs

--- public/js/index.js
+++ public/js/index.js
@@ -10,22 +10,27 @@ socket.on('disconnect', function (){
 
 socket.on('newMessage', function(message){
     let formattedTime = moment(message.createAt).format('h:mm a');
-   console.log('New Message', message); 
-    let li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createAt: formattedTime
+    });
     
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
+
 });
 
 socket.on('newLocationMessage', function(message){
-    formattedTime = moment(message.createAt).format('h:mm a');
-   let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
     
-    li.append(a);
-    jQuery('#messages').append(li);
+    formattedTime = moment(message.createAt).format('h:mm a');
+   let template = jQuery('#location-message-template').html();
+    let html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 
